41dca928d4c62fa0785477239f83a6b69747341c
Nikita <102928340+frymex@users.noreply.github.com>
2025-06-26 12:25:05 +0300

    Update settings.py

--- fakeUSGenerator/settings.py
+++ fakeUSGenerator/settings.py
@@ -120,5 +120,6 @@ STATICFILES_DIRS = [BASE_DIR / 'static']
 TEMPLATES[0]['DIRS'] = [BASE_DIR / 'templates']
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
